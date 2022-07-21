1cf29aaa51b8cdbad3e8189af205908f67f8437c
Nathan Liyodi Nariai <nathan.nariai@gmail.com>
2022-07-20 22:08:22 -0300

    Concorrencia

---
+++ 18 - Concorrencia/goroutines/goroutines.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	go escrever("Olá mundo")
+	escrever("Programando em Go")
+}
+
+func escrever(texto string) {
+	for {
+fmt.Println(texto)
+time.Sleep(time.Second)
+	}
+	
+}
