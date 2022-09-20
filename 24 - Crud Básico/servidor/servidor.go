0522b439e8b47fc49578bfe135e1311b4c70ffc8
Nathan Liyodi Nariai <nathan.nariai@gmail.com>
2022-09-20 13:04:57 -0300

    Criação da rota para atualização de usuarios

--- 24 - Crud Básico/servidor/servidor.go
+++ 24 - Crud Básico/servidor/servidor.go
@@ -129,3 +129,46 @@ func BuscarUsuario(w http.ResponseWriter, r * http.Request){
 		return
 	}
 }
+
+func AtualizarUsuario(w http.ResponseWriter, r * http.Request){
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 32)
+	if erro != nil{
+		w.Write([]byte("Erro ao converter o paramentro para inteiro"))
+		return
+	}
+
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+		if erro != nil{
+		w.Write([]byte("Erro ao ler a requisição"))
+		return
+	}
+
+	var usuario usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil{
+		w.Write([]byte("Erro ao converter usuario para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil{
+		w.Write([]byte("Erro ao conectar ao banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("update usuarios set nome = ?, email = ? where id = ?")
+		if erro != nil{
+		w.Write([]byte("Erro ao criar o statement"))
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuario.Nome, usuario.Email, ID); erro != nil{
+		w.Write([]byte("Erro ao atualizar o usuario"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
